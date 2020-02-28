1ed5df42e2c051eb369d9567077b054d7fada826
jason giroux <jaysongiroux@gmail.com>
2020-02-28 09:15:24 -0800

    adding project 2 files

---
+++ project 2/main.c
@@ -0,0 +1,118 @@
+/*************************************************************/
+/* (C) Copyright Kenneth C. Louden 1999                      */
+/*                                                           */
+/* Permission is granted for the use of this code by         */
+/* instructors in regularly scheduled classes at a college   */
+/* or university requiring my text:                          */
+/*                                                           */
+/* K. Louden, Compiler Construction: Principles and          */
+/*  Practice, PWS Publishing Co., 1997.                      */
+/*                                                           */
+/* Additionally, individual purchasers of the text may use   */
+/* this code for their own private, non-commercial purposes. */
+/* All other reproduction, distribution, or display outside  */
+/* the classroom, including posting or tranmission on the    */
+/* internet, are prohibited and constitute a violation of    */
+/* this copyright.                                           */
+/*************************************************************/
+
+/*************************************************************/
+/*   File: main.c                                            */
+/*   Main program for the C-Minus compiler                   */
+/*   Version 4 8/1/99                                        */
+/*   K. Louden, Dept. of Math & Computer Science             */
+/*   San Jose State University, San Jose, CA 95192-0103      */
+/*   (408) 924-5134   louden@cs.sjsu.edu                     */
+/*   http://www.mathcs.sjsu.edu/faculty/louden/              */
+/*************************************************************/
+
+#include "globals.h"
+
+/* set NO_PARSE to TRUE to get a scanner-only compiler */
+#define NO_PARSE FALSE
+
+/* set NO_ANALYZE to TRUE to get a parser-only compiler */
+#define NO_ANALYZE FALSE
+
+/* set NO_CODE to TRUE to get a compiler that does not
+ * generate code
+ */
+#define NO_CODE FALSE
+
+#include "util.h"
+#if NO_PARSE
+#include "scan.h"
+#else
+#include "parse.h"
+#if !NO_ANALYZE
+#include "analyze.h"
+#if !NO_CODE
+#include "cgen.h"
+#endif
+#endif
+#endif
+
+/* allocate global variables */
+int lineno = 0;
+FILE * source;
+FILE * listing;
+FILE * code;
+
+/* allocate and set tracing flags */
+int EchoSource = FALSE;
+int TraceScan = FALSE;
+int TraceParse = FALSE;
+int TraceAnalyze = FALSE;
+int TraceCode = FALSE;
+
+/* error flag to inhibit subsequent passes */
+int Error = FALSE;
+
+main( int argc, char * argv[] )
+{ TreePtr syntaxTree;
+  char pgm[120]; /* source code file name */
+  if (argc != 2)
+    { fprintf(stderr,"usage: %s <filename>\n",argv[0]);
+      exit(1);
+    }
+  strcpy(pgm,argv[1]) ;
+  if (strchr (pgm, '.') == NULL)
+     strcat(pgm,".cm");
+  source = fopen(pgm,"r");
+  if (source==NULL)
+  { fprintf(stderr,"File %s not found\n",pgm);
+    exit(1);
+  }
+  listing = stdout; /* send listing to screen */
+  fprintf(listing,"\n>>>>> C-MINUS COMPILATION: %s\n\n",pgm);
+#if NO_PARSE
+  while (getToken()!=ENDFILE);
+#else
+  syntaxTree = parse();
+#if !NO_ANALYZE
+  if (! Error)
+  { buildSymtab(syntaxTree);
+    typeCheck(syntaxTree);
+  }
+#if !NO_CODE
+  if (! Error)
+  { char * codefile;
+    int fnlen = strcspn(pgm,".");
+    codefile = (char *) calloc(fnlen+4, sizeof(char));
+    strncpy(codefile,pgm,fnlen);
+    strcat(codefile,".tm");
+    code = fopen(codefile,"w");
+    if (code == NULL)
+    { printf("Unable to open %s\n",codefile);
+      exit(1);
+    }
+    codeGen(syntaxTree,codefile);
+    fclose(code);
+  }
+#endif
+#endif
+#endif
+  fclose(source);
+  return 0;
+}
+
---
+++ project 2/scan.c
@@ -0,0 +1,240 @@
+
+/*************************************************************/
+/*   File: scan.c                                            */
+/*   The scanner implementation for the C-Minus compiler     */
+/*                                                           */
+/*                                                           */
+/*                                                           */
+/*************************************************************/
+
+#include "globals.h"
+#include "util.h"
+#include "scan.h"
+
+/* states in scanner DFA */
+typedef enum
+   { START,INLESS,INEQ,INGREATER,INNEQ,ENTERCOMMENT,
+     INCOMMENT,EXITCOMMENT,INNUM,INID,DONE }
+   StateType;
+
+/* lexeme of identifier or reserved word */
+char tokenString[MAXTOKENLEN+1];
+
+/* BUFLEN = length of the input buffer for
+   source code lines */
+#define BUFLEN 256
+
+static char lineBuf[BUFLEN]; /* holds the current line */
+static int linepos = 0; /* current position in LineBuf */
+static int bufsize = 0; /* current size of buffer string */
+static int EOF_Flag = FALSE; /* kludge for EOF without newline */
+
+/* getNextChar fetches the next non-blank character
+   from lineBuf, reading in a new line if lineBuf is
+   exhausted */
+static int getNextChar(void)
+{ if (!(linepos < bufsize))
+  { if (fgets(lineBuf,BUFLEN-1,source))
+    { lineno++;
+      bufsize = strlen(lineBuf);
+      if (EchoSource)
+      { fprintf(listing,"%4d: %s",lineno,lineBuf);
+        /* The following line makes sure a newline gets printed
+           even if it is not in the original line -- this occurs
+           only on EOF without newline */
+        if (lineBuf[bufsize-1] != '\n') fprintf(listing,"\n");
+      }
+      linepos = 0;
+      return lineBuf[linepos++];
+    }
+    else
+    { EOF_Flag = TRUE;
+      return EOF;
+    }
+  }
+  else return lineBuf[linepos++];
+}
+
+/* ungetNextChar backtracks one character
+   in lineBuf */
+static void ungetNextChar(void)
+{ if (!EOF_Flag) linepos-- ;}
+
+/* lookup table of reserved words */
+/* Alphabetic order for binary search */
+static struct
+    { char* str;
+      TokenType tok;
+    } reservedWords[MAXRESERVED]
+   = {{"else",ELSE},{"if",IF},{"int",INT},
+      {"return",RETURN},{"void",VOID},{"while",WHILE}
+     };
+
+/* lookup an identifier to see if it is a reserved word */
+/* uses binary search */
+static TokenType reservedLookup (char * s)
+{ 
+ //****************************
+ //add code here
+ //load your binary search algorithm here 
+  
+}
+
+/****************************************/
+/* the primary function of the scanner  */
+/****************************************/
+/* function getToken returns the
+ * next token in source file
+ */
+TokenType getToken(void)
+{  /* index for storing into tokenString */
+   int tokenStringIndex = 0;
+   /* holds current token to be returned */
+   TokenType currentToken;
+   /* current state - always begins at START */
+   StateType state = START;
+   /* flag to indicate save to tokenString */
+   /* FALSE case is most common -- set to TRUE
+      below if ID or NUM or ERROR */
+   int save = FALSE;
+   while (state != DONE)
+   { int c = getNextChar();
+     switch (state)
+     { case START:
+         /* order of tests reorganized a bit for better
+            efficiency */
+         if ((c == ' ') || (c == '\t') || (c == '\n'))
+           /* skip */;
+         else if (isdigit(c))
+         { save = TRUE;
+           state = INNUM;
+         }
+         else if (isalpha(c))
+         {
+			//Add code here
+			
+         }
+         else switch (c)
+         { case '<':
+             state = INLESS;
+             break;
+           
+			  //add code here
+			  
+			  
+			  default:
+             state = DONE;
+             switch (c)
+             { case EOF:
+                 currentToken = ENDFILE;
+                 break;
+               case '+':
+                
+					//Add code here
+					 
+					default:
+                 currentToken = ERROR;
+                 save = TRUE;
+                 break;
+             }
+             break;
+         }
+         break;
+       case ENTERCOMMENT:
+         if (c == '*') state = INCOMMENT;
+         else
+         { state = DONE;
+           /* backup in the input */
+          
+			 //Add code here
+			 
+			 currentToken = OVER;
+         }
+         break;
+       case INCOMMENT:
+         if (c == '*') 
+			
+			//Add code here
+			
+         else if (c == EOF)
+         { state = DONE;
+           currentToken = ENDFILE;
+         }
+         break;
+       case EXITCOMMENT:
+         if (c == '/') state = START;
+         
+			//Add code here
+			
+         break;
+       case INLESS:
+         state = DONE;
+         if (c == '=')
+           currentToken = LE;
+         else
+         { /* backup in the input */
+           ungetNextChar();
+           currentToken = LT;
+         }
+         break;
+       case INEQ:
+        
+		  //Add code here
+		  
+		  break;
+       case INGREATER:
+         state = DONE;
+         if (c == '=')
+           currentToken = GE;
+         else
+         { /* backup in the input */
+   			//Add code here       
+			}
+         break;
+       case INNEQ:
+         state = DONE;
+         if (c == '=')
+          // Add code here
+			else
+         { /* backup in the input */
+           ungetNextChar();
+           currentToken = ERROR;
+         }
+         break;
+       case INNUM:
+         if (!isdigit(c))
+         { /* backup in the input */
+           ungetNextChar();
+           save = FALSE;
+           state = DONE;
+           currentToken = NUM;
+         }
+         break;
+       case INID:
+        
+		  //Add code here
+		  
+		 break;
+		 
+       case DONE:
+       default: /* should never happen */
+         fprintf(listing,"Scanner Bug: state= %d\n",state);
+         state = DONE;
+         currentToken = ERROR;
+         break;
+     }
+     if ((save) && (tokenStringIndex <= MAXTOKENLEN))
+       tokenString[tokenStringIndex++] = (char) c;
+     if (state == DONE)
+     { tokenString[tokenStringIndex] = '\0';
+       if (currentToken == ID)
+         currentToken = reservedLookup(tokenString);
+     }
+   }
+   if (TraceScan) {
+     fprintf(listing,"\t%d: ",lineno);
+     printToken(currentToken,tokenString);
+   }
+   return currentToken;
+} /* end getToken */
+
